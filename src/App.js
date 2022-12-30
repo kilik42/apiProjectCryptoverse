a68a4fa52eaaf9e782d01936cbe19242e7a98139
kilik42 <maevins@cps.edu>
2022-12-29 23:28:11 -0600

    first commit

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import {Switch, Route, Link} from 'react-router-dom';
+import {Layout, Typography, Space} from 'antd';
+import {Navbar, Exchanges, Homepage, Cryptocurrencies, News, CryptoDetails} from './components';
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div className="navbar">
+        <Navbar />
+      </div>
+      <div className="main">
+       
+      </div>
+      <div className="footer">
+
+      </div>
+
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/index.js
@@ -0,0 +1,15 @@
+import {default as Navbar} from './Navbar';
+import {default as Exchanges} from './Exchanges';
+import {default as Homepage} from './Homepage';
+import {default as Cryptocurrencies} from './Cryptocurrencies';
+import {default as News} from './News';
+import {default as CryptoDetails} from './CryptoDetails';
+
+export {
+    Navbar,
+    Exchanges,
+    Homepage,
+    Cryptocurrencies,
+    News,
+    CryptoDetails
+}
