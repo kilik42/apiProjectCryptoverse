3a67c2021d92a92105c784a7834b9b5119c58050
kilik42 <maevins@cps.edu>
2022-12-31 01:20:12 -0600

    fixed router problem

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {Switch, Route, Link} from 'react-router-dom';
+import {BrowserRouter as Router, Routes, Route, Link} from 'react-router-dom';
 import {Layout, Typography, Space} from 'antd';
 import {Navbar, Exchanges, Homepage, Cryptocurrencies, News, CryptoDetails} from './components';
 
@@ -12,27 +12,24 @@ function App() {
         <Navbar />
       </div>
       <div className="main">
-       <Layout >
-          <div className="routes">
-            <Switch>
-              <Route exact path="/">
-                <Homepage />
-              </Route>
-              <Route exact path="/exchanges">
-                <Exchanges />
-              </Route>
-              <Route exact path="/cryptocurrencies">
-                <Cryptocurrencies />
-              </Route>
-              <Route exact path="/crypto/:coinId">
-                <CryptoDetails />
-              </Route>
-              <Route exact path="/news">
-                <News />
-              </Route>
-            </Switch>
-          </div>
-       </Layout>
+        
+            
+                <div className="routes">
+                  <Routes>
+                    <Route exact path="/" component={Homepage}/>
+                      
+                    <Route exact path="/exchanges" component={Exchanges}/>
+                  
+                    <Route exact path="/cryptocurrencies" component={Cryptocurrencies}/>
+                    
+                    <Route exact path="/crypto/:coinId" component={CryptoDetails}/>
+                      
+                    <Route exact path="/news" component={News}/>
+                      
+                  </Routes>
+                </div>
+            
+       
       </div>
       <div className="footer">
 
